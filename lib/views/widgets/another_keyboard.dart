5b6b7d95678b815188c30135ef54c889e02c59a4
sparkwud <kelvinagbenyo@gmail.com>
2024-05-09 00:31:52 +0000

    keyboard placeholder wip

---
+++ lib/views/widgets/another_keyboard.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class AnotherKeyboard extends StatelessWidget {
+  const AnotherKeyboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
